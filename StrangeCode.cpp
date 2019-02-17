7102ab0d28d66ec3456484cf2320fa0d49211a31
大爷 蔡 <1152545264@qq.com>
2019-02-17 16:14:34 +0800

    再次测试更改

--- StrangeCode.cpp
+++ StrangeCode.cpp
@@ -1,15 +1,15 @@
 //利用lambda表达式在main函数前打印一些信息
 #include<iostream>
 using namespace std;
-
-int a = []()->int
-    {
-      cout<<"在main函数之前执行的代码"<<endl;
-      return 0;
-    }();
+int a = []()->int{
+	cout << "Before main Function"<<endl;
+	//不要使用中文字符串，不然编译提示错误：常量中有换行符
+	//我也暂时没找到解决方案，属性里面改字符集不奏效
+	return 0;
+}();
  
  int main()
 {
-  cout<<"在main函数之后执行"<<endl;
+  cout<<"Atfer Main Function"<<endl;
   return 0;
 }
